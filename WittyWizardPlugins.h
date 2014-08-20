21f2eb128e3f9897e7a4d18e07a29054fb01d0e3
WittyWizard <admin@wittywizard.org>
2014-08-20 15:03:10 -0700

    update WittyWizardPlugins.h

---
+++ WittyWizardPlugins.h
@@ -0,0 +1,13 @@
+/* ****************************************************************************
+ * Plugin System: A modern plugin system is needed, this is a temporary fix
+ * Witty Wizard
+ */
+#ifdef HITCOUNTERMAN
+    #include "view/HitCounterManView.h"
+#endif
+#ifdef VIDEOMAN
+    #include "view/VideoManView.h"
+#endif
+#ifdef VIDEOMAN
+    #include "view/VideoManView.h"
+#endif
