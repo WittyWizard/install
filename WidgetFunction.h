b488f342676fc6ece195ecd2604734abdc9f3b78
WittyWizard <admin@wittywizard.org>
2014-09-22 16:17:25 -0700

    update WidgetFunction.h

--- WidgetFunction.h
+++ WidgetFunction.h
@@ -6,13 +6,23 @@
 #include <Wt/WLineEdit>
 #include <Wt/WAudio>
 #include <Wt/WMediaPlayer>
+#include <Wt/WVideo>
+#include <Wt/WComboBox>
 #include <Wt/WText>
 #include <Wt/WLink>
+#include <Wt/WImage>
+#include <Wt/WCheckBox>
+#include <Wt/WLineEdit>
+#include <Wt/WPushButton>
+#include <Wt/WHBoxLayout>
+#include <Wt/WToolBar>
+//#include <Wt/WIntValidator>
 #include <boost/function.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
 //#include <boost/lexical_cast.hpp>
 //#include <boost/tokenizer.hpp>
+#include <map>
 /* ****************************************************************************
  * Widget Function
  */
@@ -30,17 +40,21 @@ class WidgetFunction
         bool typeFound = false;
 
         static Wt::WWidget* createAudio(const std::vector<Wt::WString>& args);
+        static Wt::WWidget* createVideo(const std::vector<Wt::WString>& args);
         static Wt::WWidget* createLineEdit(const std::vector<Wt::WString>& args);
         static Wt::WWidget* getFlashBack(Wt::WContainerWidget *result, std::string tagId, std::string tagSrc, std::string tagAltSrc, std::string tagClass, std::string tagWidth, std::string tagHeight, std::string tagTitle, std::string tagAlign);
+        static Wt::WPushButton* createButton(const Wt::WString& text, const std::string className, const std::string link);
 
     private:
         typedef std::map<std::string, InstatiateWidget> RegistryMap;
         RegistryMap registeredTypes_;
         Wt::WTemplate* myTemplate_;
         bool debugLog = true;
+
         //
         static bool isFile(std::string fileName);
         static std::string getArg(const std::string& name, const std::vector<Wt::WString>& args);
+        static bool StringReplace(std::string& string2replace, const std::string& changefrom, const std::string& changeTo);
 }; // end class WidgetFunction
 #endif // WIDGETFUNCTION_H
 // --- End Of File ------------------------------------------------------------
